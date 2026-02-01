7f6879f82629e1c291d89ca0284d35b0f6cd1e0d
saiteja <saiteja299@gmail.com>
2026-02-01 17:24:50 +0530

    Add GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import ScrollToTop from './components/utils/ScrollToTop';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
